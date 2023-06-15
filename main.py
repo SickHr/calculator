fa696abc8c69fa207822c07ef28e10e5e9040a26
SickHr <blawatjustin@gmail.com>
2023-06-15 14:05:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,44 @@
-import tkinter as tk
-from tkinter import Entry, Button
+import customtkinter as ctk
 
-# Function to execute when a button is pressed
-def on_button_press(button_text):
-    current_text = entry.get()
-    if button_text == "=":
-        try:
-            result = eval(current_text)
-            entry.delete(0, tk.END)
-            entry.insert(tk.END, str(result))
-        except:
-            entry.delete(0, tk.END)
-            entry.insert(tk.END, "Error")
-    elif button_text == "C":
-        entry.delete(0, tk.END)
-    else:
-        entry.insert(tk.END, button_text)
-
-# Create the main window
-root = tk.Tk()
+# Root window
+root = ctk.CTk()
 root.title("Calculator")
-root.geometry("260x310")
-root.resizable(0, 0)  # Make the window non-resizable
+root.geometry("345x350")
+root.resizable(False, False)
 
-# Entry widget for user input
-entry = Entry(root, width=16, font=("Arial", 18), borderwidth=5)
-entry.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
-# Define buttons and their corresponding text
-buttons_text = [
-    ('7', 1, 0), ('8', 1, 1), ('9', 1, 2),
-    ('4', 2, 0), ('5', 2, 1), ('6', 2, 2),
-    ('1', 3, 0), ('2', 3, 1), ('3', 3, 2),
-    ('0', 4, 1), ('+', 1, 3), ('-', 2, 3),
-    ('*', 3, 3), ('/', 4, 3), ('=', 4, 2),
-    ('C', 4, 0)
+def on_button_press(button_text):
+    current_text = entry1.get()
+    if button_text == "=":
+
+        eval(current_text)
+        result = eval(current_text)
+        entry1.delete(0, "end")
+        entry1.insert("end", result)
+
+    elif button_text == "c":
+        entry1.delete(0, "end")
+    else:
+        entry1.delete(0, "end")
+        entry1.insert("end", current_text + button_text)
+
+
+# entry
+entry1 = ctk.CTkEntry(root, width=200, font=("Arial", 20))
+entry1.grid(column=0, row=0, columnspan=5, padx=51, pady=51)
+
+# Display
+buttons_position = [
+    ("7", 1, 0), ("8", 1, 1), ("9", 1, 2), ("c", 1, 3),
+    ("4", 2, 0), ("5", 2, 1), ("6", 2, 2), ("+", 2, 3),
+    ("1", 3, 0), ("2", 3, 1), ("3", 3, 2), ("-", 3, 3),
+    ("0", 4, 0), (".", 4, 1), ("=", 4, 2), ("*", 4, 3)
+
 ]
 
-# Create buttons and add them to the main window
-for button_text, row, column in buttons_text:
-    button = Button(root, text=button_text, width=5, height=2, font=("Arial", 13),
-                    bg='#f2f2f2', command=lambda b=button_text: on_button_press(b))
-    button.grid(row=row, column=column, padx=5, pady=5)
+for text, row, column in buttons_position:
+    button = ctk.CTkButton(root, text=text, width=75, height=40, font=("Arial", 20), command=lambda text=text: on_button_press(text))
+    button.grid(column=column, row=row, padx=5, pady=5)
 
-# Start the main event loop
-root.mainloop()
+if __name__ == '__main__':
+    root.mainloop()
